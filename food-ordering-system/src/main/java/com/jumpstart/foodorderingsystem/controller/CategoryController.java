78854bcb8c515c2b434814df28494d8349537bee
Busi202 <busibabee@gmail.com>
2026-06-12 08:53:32 +0200

    Debugging CategoryController.java

--- food-ordering-system/src/main/java/com/jumpstart/foodorderingsystem/controller/CategoryController.java
+++ food-ordering-system/src/main/java/com/jumpstart/foodorderingsystem/controller/CategoryController.java
@@ -1,14 +1,16 @@
-package food_ordering_system.controller;
-/*import com.jumpstart.foodorderingsystem.entity.Category;
+package com.jumpstart.foodorderingsystem.controller;
+
+import com.jumpstart.foodorderingsystem.entity.Category;
 import com.jumpstart.foodorderingsystem.service.CategoryService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/category")*/
+@RequestMapping("/api/category")
 public class CategoryController {
-    /*private final CategoryService categoryService;
+
+    private final CategoryService categoryService;
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
@@ -17,5 +19,5 @@ public class CategoryController {
     @GetMapping
     public List<Category> getCategories() {
         return categoryService.getAllCategories();
-    }*/
+    }
 }
