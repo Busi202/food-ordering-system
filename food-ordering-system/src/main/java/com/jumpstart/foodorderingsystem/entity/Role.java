9a56141b0fc80f9536020f5d00ea8c505a82c6c0
Busi202 <busibabee@gmail.com>
2026-07-14 10:00:36 +0200

    Feature(role): Create Role entity

---
+++ food-ordering-system/src/main/java/com/jumpstart/foodorderingsystem/entity/Role.java
@@ -0,0 +1,20 @@
+package com.jumpstart.foodorderingsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
